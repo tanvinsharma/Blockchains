31e8b1924a3c825bf1345ce37d150caef9ebc78a
tanvinsharma <tanvin2000s@gmail.com>
2021-12-05 01:15:48 +0100

    sample blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,48 @@
-print('test')
+import hashlib
+
+class CoinBlock:
+
+	def __init__(self, previous_block_hash, transaction_list):
+
+		self.previous_block_hash = previous_block_hash
+		self.transaction_list = transaction_list
+
+		self.block_data = f"{' - '.join(transaction_list)} - {previous_block_hash}"
+		self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
+
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.generate_genesis_block()
+
+    def generate_genesis_block(self):
+        self.chain.append(CoinBlock("0", ['Genesis Block']))
+    
+    def create_block_from_transaction(self, transaction_list):
+        previous_block_hash = self.last_block.block_hash
+        self.chain.append(CoinBlock(previous_block_hash, transaction_list))
+
+    def display_chain(self):
+        for i in range(len(self.chain)):
+            print(f"Data {i + 1}: {self.chain[i].block_data}")
+            print(f"Hash {i + 1}: {self.chain[i].block_hash}\n")
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+t1 = "George sends 3.1 GC to Joe"
+t2 = "Joe sends 2.5 GC to Adam"
+t3 = "Adam sends 1.3 GC to Bob"
+t4 = "Bob sends 0.5 GC to Charlie"
+t5 = "Charlie sends 0.2 GC to David"
+t6 = "David sends 0.1 GC to Eric"
+
+myblockchain = Blockchain()
+
+myblockchain.create_block_from_transaction([t1, t2])
+myblockchain.create_block_from_transaction([t3, t4])
+myblockchain.create_block_from_transaction([t5, t6])
+
+myblockchain.display_chain()
